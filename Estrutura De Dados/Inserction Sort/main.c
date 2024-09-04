7d6b3f5eed0974f7bce24207eff800ec6ae33cee
Matheus Endlich Silveira <126157948+MatheusIngles@users.noreply.github.com>
2024-09-04 08:43:32 -0300

    Add files via upload

---
+++ Estrutura De Dados/Inserction Sort/main.c
@@ -0,0 +1,42 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+#define n 10
+
+int print(int *v){
+    for(int i = 0;i < n; i++){
+        printf("%d ",v[i]);
+    }
+    printf("\n");
+}
+int insetionsort(int *v){
+    int troca = 0;
+    for(int i = 0; i < n-1;i++){
+        for(int j = i+1;j>0;j--){
+            if(v[j]< v[j-1]){
+                troca = v[j];
+                v[j] = v[j-1];
+                v[j - 1] = troca;
+            }
+        }
+    }
+    print(v);
+}
+
+
+int main()
+{
+    int bosta[n] = {1,2,3,4,5,99,7,8,9,10};
+    print(bosta);
+    insetionsort(bosta);
+
+    return 0;
+}
